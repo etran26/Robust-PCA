870104e953da07c9bcfcd4895dcc0fb4b857db66
trane2 <trane2@rpi.edu>
2020-04-30 02:50:50 -0400

    wednsdeat NIght

--- RPCA_data_testFile/alm_solver.m
+++ RPCA_data_testFile/alm_solver.m
@@ -4,7 +4,7 @@ function [X, L, S, Y, Z, err, iter] = alm_solver(X)
     unobserved = isnan(X);
     lambda = 1 / sqrt(max(r, c));
     mu = 10 * lambda;
-    tol = 1e-6;
+    tol = 1e-4;
     normX = norm(X, 'fro');
     L = zeros(r, c);
     LNot = zeros(r, c);
@@ -16,21 +16,32 @@ function [X, L, S, Y, Z, err, iter] = alm_solver(X)
     tNot = 1;
     
     iter = 0;
-    err = 1e-7;
-    while err < tol || iter < 500
-        iter = iter + 1;
+    err = 1;
+    err2 = 1e-7;
+    stoppingCrit = 1e-7;
+    while err > tol
+        iter = iter + 1
         
         YL = L + ((tNot - 1) / t) * (L - LNot);
         YS = S + ((tNot - 1) / t) * (S - SNot);
         GL = YL - 0.5 * (YL + YS - X);
+        LNot = L;
+        SNot = S;
         L = Do(mu / 2, GL);
         GS = YS - 0.5 * (YL + YS - X);
         S = So((lambda * mu) / 2, GS);
+        tNot = t;
         t = (1 + sqrt((4 * (t^2)) + 1)) / 2;
-        mu = max(mu, 0);
+        mu = max(.99 * mu, 0);
+        %mu = max(mu, 0);
         Z = X - L - S;
         Z(unobserved) = 0;
-        err = norm(Z, 'fro') / normX;
+        err = norm(Z, 'fro') / normX
+        %err2 = min(mu, sqrt(mu)) * norm((S - SNot), 'fro') / normX;
+        %stoppingCritL = 2 * (YL - L) + (L + S - YL - YS);
+        %stoppingCritS = 2 * (YS - S) + (L + S - YL - YS);
+        %stoppingCrit = sqrt(norm(stoppingCritL, 'fro')^2 + norm(stoppingCritS, 'fro')^2)
+        
         
     end
 
--- RPCA_data_testFile/test_model_RPCA.m
+++ RPCA_data_testFile/test_model_RPCA.m
@@ -12,8 +12,8 @@ Lopt = S.Lopt;
 Sopt = S.Sopt;
 
 % call your solver to obtain (L, S)
-%[X, L, S, Y, Z, err, iter] = admm_solver(X);
-[X, L, S, Y, Z, err, iter] = alm_solver(X);
+[X, L, S, Y, Z, err, iter] = admm_solver(X);
+%[X, L, S, Y, Z, err, iter] = alm_solver(X);
 
 fprintf('||L-Lopt||/||Lopt|| = %5.4e\n',norm(L-Lopt,'fro')/norm(Lopt,'fro'));
 fprintf('||S-Sopt||/||Sopt|| = %5.4e\n\n',norm(S-Sopt,'fro')/norm(Sopt,'fro'));
@@ -34,8 +34,8 @@ end
 
 % call your solver to obtain (L, S)
 
-%[X, L, S, Y, Z, err, iter] = admm_solver(X);
-[X, L, S, Y, Z, err, iter] = alm_solver(X);
+[X, L, S, Y, Z, err, iter] = admm_solver(X);
+%[X, L, S, Y, Z, err, iter] = alm_solver(X);
 fprintf('See Output Images\n')
 
 % show a few slices
