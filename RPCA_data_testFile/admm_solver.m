9eb730e545d12a045a7e8c1fe82db285460a508c
trane2 <trane2@rpi.edu>
2020-05-01 21:39:09 -0400

    Code cleanup/comments

--- RPCA_data_testFile/admm_solver.m
+++ RPCA_data_testFile/admm_solver.m
@@ -1,5 +1,5 @@
 function [X, L, S, Y, Z, res, iter, objs, cv] = admm_solver(X)
-
+    % initialize variables
     [r, c] = size(X);
     unobserved = isnan(X);
     X(unobserved) = 0;
@@ -13,30 +13,32 @@ function [X, L, S, Y, Z, res, iter, objs, cv] = admm_solver(X)
     cv = [1];
     iter = 0;
     res = 1;
+    % stopping condition
     while res > tol
         iter = iter + 1;
-        L = Do(1 / mu, X - S + (1 / mu) * Y);
-        S = So(lambda / mu, X - L + (1 / mu) * Y);
+        % subproblems
+        L = SOSingular(1 / mu, X - S + (1 / mu) * Y);
+        S = SO(lambda / mu, X - L + (1 / mu) * Y);
         Z = X - L - S;
         Z(unobserved) = 0;
         Y = Y + mu * Z;
+        % primal feasibility
         res = norm(Z, 'fro') / norm(X, 'fro');
-        % mu = max(eta * mu, mu_bar);
         objs = [objs; Obj(L,S,lambda)];
         cv = [cv; res];
     end
 
 end
 
-function r = So(tau, X)
+function r = SO(tau, X)
     % shrinkage operator
     r = sign(X) .* max(abs(X) - tau, 0);
 end
 
-function r = Do(tau, X)
-    % shrinkage operator for singular values
+function r = SOSingular(tau, X)
+    % shrinkage operator (singular values case)
     [U, S, V] = svd(X, 'econ');
-    r = U*So(tau, S)*V';
+    r = U*SO(tau, S)*V';
 end
 
 function r = Obj(L, S, lambda)
--- RPCA_data_testFile/penalty_solver.m
+++ RPCA_data_testFile/penalty_solver.m
@@ -1,5 +1,5 @@
 function [X, L, S, Y, Z, res, iter, objs, cv] = penalty_solver(X)
-
+    % initialize variables
     [r, c] = size(X);
     unobserved = isnan(X);
     lambda = 1 / sqrt(max(r, c));
@@ -16,42 +16,47 @@ function [X, L, S, Y, Z, res, iter, objs, cv] = penalty_solver(X)
     objs = [Obj(L,S, lambda)];
     cv = [1];
     tNot = 1;
-    
     iter = 0;
     res = 1;
     err = 1;
+    % stopping condition
     while (res > tol || err > tol) && mu_bar < mu
         iter = iter + 1;
+        % subproblems (closed form)
         YL = L + ((tNot - 1) / t) * (L - LNot);
         YS = S + ((tNot - 1) / t) * (S - SNot);
         GL = YL - 0.5 * (YL + YS - X);
         LNot = L;
         SNot = S;
-        L = Do(mu / 2, GL);
+        L = SOSingular(mu / 2, GL);
         GS = YS - 0.5 * (YL + YS - X);
-        S = So((lambda * mu) / 2, GS);
+        S = SO((lambda * mu) / 2, GS);
         tNot = t;
         t = (1 + sqrt((4 * (t^2)) + 1)) / 2;
+        % update mu
         mu = max(eta * mu, mu_bar);
         Z = X - L - S;
         Z(unobserved) = 0;
+        % primal feasibility
         res = norm(Z, 'fro') / norm(X, 'fro');
+        % dual feasibility
         err = max(mu, sqrt(mu)) * norm((S - SNot), 'fro') / norm(X, 'fro');
+        % store objective and constraint violation for graphing
         objs = [objs; Obj(L,S,lambda)];
         cv = [cv; res];
     end
 
 end
 
-function r = So(tau, X)
+function r = SO(tau, X)
     % shrinkage operator
     r = sign(X) .* max(abs(X) - tau, 0);
 end
 
-function r = Do(tau, X)
-    % shrinkage operator for singular values
+function r = SOSingular(tau, X)
+    % shrinkage operator (singular values case)
     [U, S, V] = svd(X, 'econ');
-    r = U*So(tau, S)*V';
+    r = U*SO(tau, S)*V';
 end
 
 function r = Obj(L, S, lambda)
